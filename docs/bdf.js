5de85bf227fe874bf787953bcab9ee3dbbccb769
Lee Minseo <itoupluk427@gmail.com>
2022-01-01 01:02:25 +0900

    Update bdf.js

--- docs/bdf.js
+++ docs/bdf.js
@@ -1,14 +1,14 @@
-if (window.location.endsWith('galmuri11')) {
+if (location.endsWith('galmuri11')) {
   fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11.bdf')
     .then(function(response) {
       console.log(response.match(/STARTCHAR U+(....)/g));
     });
-} else if (window.location.endsWith('galmuri11-bold')) {
+} else if (location.endsWith('galmuri11-bold')) {
   fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11-Bold.bdf')
     .then(function(response) {
       console.log(response.match(/STARTCHAR U+(....)/g));
     });
-} else if (window.location.endsWith('galmuri9')) {
+} else if (location.endsWith('galmuri9')) {
   fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri9.bdf')
     .then(function(response) {
       console.log(response.match(/STARTCHAR U+(....)/g));
