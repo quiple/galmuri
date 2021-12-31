61fee1edc914ece06ee4eb47e2ff0d0a7f1bf323
Lee Minseo <itoupluk427@gmail.com>
2022-01-01 00:54:38 +0900

    Update bdf.js

--- docs/bdf.js
+++ docs/bdf.js
@@ -1,8 +1,16 @@
-let reader = new FileReader();
-let glphs = [];
-reader.addEventListener('load', event => {
-  glphs = event.target.result.match(/STARTCHAR U+(....)/g);
-  console.log(glphs);
-});
-console.log(window.location);
-reader.readAsText('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11.bdf');
+if (window.location.endsWith('galmuri11')) {
+  fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11.bdf')
+    .then(function(response) {
+      console.log(response.match(/STARTCHAR U+(....)/g));
+    });
+} else if (window.location.endsWith('galmuri11-bold')) {
+  fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11-Bold.bdf')
+    .then(function(response) {
+      console.log(response.match(/STARTCHAR U+(....)/g));
+    });
+} else if (window.location.endsWith('galmuri9')) {
+  fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri9.bdf')
+    .then(function(response) {
+      console.log(response.match(/STARTCHAR U+(....)/g));
+    });
+}
