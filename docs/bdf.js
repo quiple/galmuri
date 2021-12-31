eed655dc617a5befdca39eac140c6836044f5bc9
Lee Minseo <itoupluk427@gmail.com>
2022-01-01 02:55:40 +0900

    폰트 스타일 설정

--- docs/bdf.js
+++ docs/bdf.js
@@ -1,9 +1,13 @@
 if (location.toString().endsWith('galmuri11')) {
   var fileName = 'Galmuri11';
+  document.getElementById('glyphs').style.fontFamily = 'Galmuri11';
 } else if (location.toString().endsWith('galmuri11-bold')) {
   var fileName = 'Galmuri11-Bold';
+  document.getElementById('glyphs').style.fontFamily = 'Galmuri11';
+  document.getElementById('glyphs').style.fontWeight = '700';
 } else if (location.toString().endsWith('galmuri9')) {
   var fileName = 'Galmuri9';
+  document.getElementById('glyphs').style.fontFamily = 'Galmuri9';
 }
 
 fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/'+ fileName +'.bdf')
