a367886a360d5c69282166a6451a658ee11fbb28
Lee Minseo <itoupluk427@gmail.com>
2022-01-01 02:48:16 +0900

    글리프 목록

--- docs/bdf.js
+++ docs/bdf.js
@@ -1,16 +1,19 @@
-if (location.endsWith('galmuri11')) {
-  fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11.bdf')
-    .then(function(response) {
-      console.log(response.match(/STARTCHAR U+(....)/g));
-    });
-} else if (location.endsWith('galmuri11-bold')) {
-  fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri11-Bold.bdf')
-    .then(function(response) {
-      console.log(response.match(/STARTCHAR U+(....)/g));
-    });
-} else if (location.endsWith('galmuri9')) {
-  fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/Galmuri9.bdf')
-    .then(function(response) {
-      console.log(response.match(/STARTCHAR U+(....)/g));
-    });
+if (location.toString().endsWith('galmuri11')) {
+  var fileName = 'Galmuri11';
+} else if (location.toString().endsWith('galmuri11-bold')) {
+  var fileName = 'Galmuri11-Bold';
+} else if (location.toString().endsWith('galmuri9')) {
+  var fileName = 'Galmuri9';
 }
+
+fetch('https://cdn.jsdelivr.net/gh/quiple/galmuri/'+ fileName +'.bdf')
+  .then(async function(response) {
+    const text = await response.text();
+    let glphsCnt = text.match(/CHARS .*/g)[0].split(' ');
+    let glphsArr = text.match(/STARTCHAR U\+.*/g);
+    var glphs;
+    for (let i = 0; i < glphsCnt[1]; i++) {
+      glphs += String.fromCodePoint(parseInt(glphsArr[i].split('+')[1], 16));
+    }
+    document.getElementById('glyphs').innerHTML = glphs;
+  });
---
+++ docs/galmuri9.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Galmuri9
+---
+
+<link rel="stylesheet" href="./style.css">
+
+<div id="glyphs"></div>
+
+<script src="./bdf.js"></script>
