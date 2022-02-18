2067f517b1a284ddc70f86ee815d691a08562fe5
Lee Minseo <itoupluk427@gmail.com>
2022-02-18 21:44:44 +0900

    Update bdf.js

--- bdf.js
+++ bdf.js
@@ -1,16 +1,16 @@
 if (location.toString().endsWith('galmuri11')) {
   var fileName = 'Galmuri11';
-  document.getElementById('glyphs').style.fontFamily = 'Galmuri11';
+  document.getElementById('glyphs11').style.fontFamily = 'Galmuri11';
 } else if (location.toString().endsWith('galmuri11-bold')) {
   var fileName = 'Galmuri11-Bold';
-  document.getElementById('glyphs').style.fontFamily = 'Galmuri11';
-  document.getElementById('glyphs').style.fontWeight = '700';
+  document.getElementById('glyphs11').style.fontFamily = 'Galmuri11';
+  document.getElementById('glyphs11').style.fontWeight = '700';
 } else if (location.toString().endsWith('galmuri9')) {
   var fileName = 'Galmuri9';
-  document.getElementById('glyphs').style.fontFamily = 'Galmuri9';
+  document.getElementById('glyphs9').style.fontFamily = 'Galmuri9';
 } else if (location.toString().endsWith('yamche')) {
   var fileName = 'Yamche';
-  document.getElementById('glyphs').style.fontFamily = 'Yamche';
+  document.getElementById('glyphsY').style.fontFamily = 'Yamche';
 }
 
 fetch('./'+ fileName +'.bdf')
