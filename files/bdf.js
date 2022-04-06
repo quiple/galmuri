b0d1b79c3564a753d9808aced4853a6c2fcaf645
Lee Minseo <itoupluk427@gmail.com>
2022-04-06 11:24:23 +0900

    일단 도중 커밋
    BDF 폰트 파일이 필요해서...

--- files/bdf.js
+++ files/bdf.js
@@ -1,21 +1,13 @@
-if (location.toString().endsWith('galmuri11')) {
-  var fileName = 'Galmuri11';
-} else if (location.toString().endsWith('galmuri11-bold')) {
-  var fileName = 'Galmuri11-Bold';
-} else if (location.toString().endsWith('galmuri9')) {
-  var fileName = 'Galmuri9';
-} else if (location.toString().endsWith('yamche')) {
-  var fileName = 'Yamche';
-}
+const fileName = document.querySelector('input[name=fonts]:checked').value;
 
-fetch('./'+ fileName +'.bdf')
+fetch('../dist/'+ fileName +'.bdf')
   .then(async function(response) {
     const text = await response.text();
     if (window.Worker) {
       const worker = new Worker('./worker.js');
       worker.postMessage(text);
       worker.onmessage = function (glphs) {
-        document.getElementById('glyphs').innerHTML = glphs.data;
+        document.getElementById('list').innerHTML = glphs.data;
       }
     } else {
       alert('Your browser does not support Web Workers.');
