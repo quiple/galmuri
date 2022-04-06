0277ee81323982de3cf1cd87b3eabebd332f3f4d
Lee Minseo <itoupluk427@gmail.com>
2022-04-06 13:57:31 +0900

    Update style.css

--- files/bdf.js
+++ files/bdf.js
@@ -1,9 +1,19 @@
 const font = document.getElementsByClassName('btn');
+const list = document.getElementById('list');
 
 for (let i = 0; i < font.length; i++) {
   font[i].addEventListener('click', event => {
     var fileName = font[i].innerHTML;
-    if (fileName == 'Galmuri11 Bold') var fileName = 'Galmuri11-Bold';
+    if (fileName == 'Galmuri11') {
+      list.classList.add('g11');
+    } else if (fileName == 'Galmuri11 Bold') {
+      var fileName = 'Galmuri11-Bold';
+      list.classList.add('g11b');
+    } else if (fileName == 'Galmuri9') {
+      list.classList.add('g9');
+    } else if (fileName == 'Galmuri7') {
+      list.classList.add('g7');
+    }
     fetch('./dist/' + fileName + '.bdf').then(async function (response) {
       const text = await response.text();
       if (window.Worker) {
