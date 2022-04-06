af248c8848075a754982e4201d3daada3b6d3c49
Lee Minseo <itoupluk427@gmail.com>
2022-04-06 13:37:52 +0900

    Update worker.js

--- files/worker.js
+++ files/worker.js
@@ -1,8 +1,8 @@
 onmessage = function (text) {
   var glphsCnt = text.data.match(/CHARS .*/g)[0].split(' ');
   var glphsArr = text.data.match(/STARTCHAR U\+.*/g);
-  var glphs;
-  for (let i = 0; i < glphsCnt[1]; i++) {
+  var glphs = '<div class="item">' + String.fromCodePoint(parseInt(glphsArr[0].split('+')[1], 16)) + '</div>';
+  for (let i = 1; i < glphsCnt[1]; i++) {
     glphs += '<div class="item">' + String.fromCodePoint(parseInt(glphsArr[i].split('+')[1], 16)) + '</div>';
   }
   postMessage(glphs);
