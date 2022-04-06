d09ae40132ea463f7cd84c3f6b0e701539db393c
Lee Minseo <itoupluk427@gmail.com>
2022-04-06 14:20:13 +0900

    Add Characters List

--- files/bdf.js
+++ files/bdf.js
@@ -6,13 +6,25 @@ for (let i = 0; i < font.length; i++) {
     var fileName = font[i].innerHTML;
     if (fileName == 'Galmuri11') {
       list.classList.add('g11');
+      list.classList.remove('g11b');
+      list.classList.remove('g9');
+      list.classList.remove('g7');
     } else if (fileName == 'Galmuri11 Bold') {
       var fileName = 'Galmuri11-Bold';
       list.classList.add('g11b');
+      list.classList.remove('g11');
+      list.classList.remove('g9');
+      list.classList.remove('g7');
     } else if (fileName == 'Galmuri9') {
       list.classList.add('g9');
+      list.classList.remove('g11');
+      list.classList.remove('g11b');
+      list.classList.remove('g7');
     } else if (fileName == 'Galmuri7') {
       list.classList.add('g7');
+      list.classList.remove('g11');
+      list.classList.remove('g11b');
+      list.classList.remove('g9');
     }
     fetch('./dist/' + fileName + '.bdf').then(async function (response) {
       const text = await response.text();
