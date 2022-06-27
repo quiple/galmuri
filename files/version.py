8b2c5c9a5b300e7bedf3a0545eb511a653ee7a4c
Lee Minseo <itoupluk427@gmail.com>
2022-06-27 10:26:00 +0900

    Update

--- files/version.py
+++ files/version.py
@@ -7,10 +7,10 @@ b = sys.argv[1]
 f = sys.argv[2]
 
 with open(b, 'rt', encoding='utf8') as before:
-  mod = re.findall(r'FONT_VERSION\s(\d+\.\d+\.\d+)', before.read())
+  mod = re.findall(r'FontRevision\s+(\d\.\d{3})', before.read())
 
 with open(f, 'rt', encoding='utf8') as after:
-  ver = re.sub(r'"version": "(\d+\.\d+\.\d+)"', '"version": "' + mod[0] + '"', after.read())
+  ver = re.sub(r'"version": "(\d+\.\d+\.\d+)"', '"version": "' + mod[0][:4] + '.' + mod[0][-1:] + '"', after.read())
 
 with open(f, 'wt', encoding='utf8') as write:
   write.write(ver)
