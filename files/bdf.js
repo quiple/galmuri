69c5f34986f6c9c10fa4f57facc2e60150c7567d
Lee Minseo <itoupluk427@gmail.com>
2022-10-21 23:39:28 +0900

    loading..?

--- files/bdf.js
+++ files/bdf.js
@@ -2,105 +2,112 @@ const font = document.getElementsByClassName('btn');
 const list = document.getElementById('list');
 const tameshi = document.getElementById('tameshi');
 
-for (let i = 0; i < font.length; i++) {
-  font[i].addEventListener('click', event => {
-    var fileName = font[i].innerHTML;
-    if (fileName == 'Galmuri14') {
-      list.classList.add('g14');
-      list.classList.remove('g11');
-      list.classList.remove('g11b');
-      list.classList.remove('g11c');
-      list.classList.remove('g9');
-      list.classList.remove('g7');
-      tameshi.classList.add('g14');
-      tameshi.classList.remove('g11');
-      tameshi.classList.remove('g11b');
-      tameshi.classList.remove('g11c');
-      tameshi.classList.remove('g9');
-      tameshi.classList.remove('g7');
-    } else if (fileName == 'Galmuri11') {
-      list.classList.add('g11');
-      list.classList.remove('g14');
-      list.classList.remove('g11b');
-      list.classList.remove('g11c');
-      list.classList.remove('g9');
-      list.classList.remove('g7');
-      tameshi.classList.add('g11');
-      tameshi.classList.remove('g14');
-      tameshi.classList.remove('g11b');
-      tameshi.classList.remove('g11c');
-      tameshi.classList.remove('g9');
-      tameshi.classList.remove('g7');
-    } else if (fileName == 'Galmuri11 Bold') {
-      var fileName = 'Galmuri11-Bold';
-      list.classList.add('g11b');
-      list.classList.remove('g14');
-      list.classList.remove('g11');
-      list.classList.remove('g11c');
-      list.classList.remove('g9');
-      list.classList.remove('g7');
-      tameshi.classList.add('g11b');
-      tameshi.classList.remove('g14');
-      tameshi.classList.remove('g11');
-      tameshi.classList.remove('g11c');
-      tameshi.classList.remove('g9');
-      tameshi.classList.remove('g7');
-    } else if (fileName == 'Galmuri11 Condensed') {
-      var fileName = 'Galmuri11-Condensed';
-      list.classList.add('g11c');
-      list.classList.remove('g14');
-      list.classList.remove('g11');
-      list.classList.remove('g11b');
-      list.classList.remove('g9');
-      list.classList.remove('g7');
-      tameshi.classList.add('g11c');
-      tameshi.classList.remove('g14');
-      tameshi.classList.remove('g11');
-      tameshi.classList.remove('g11b');
-      tameshi.classList.remove('g9');
-      tameshi.classList.remove('g7');
-    } else if (fileName == 'Galmuri9') {
-      list.classList.add('g9');
-      list.classList.remove('g14');
-      list.classList.remove('g11');
-      list.classList.remove('g11b');
-      list.classList.remove('g11c');
-      list.classList.remove('g7');
-      tameshi.classList.add('g9');
-      tameshi.classList.remove('g14');
-      tameshi.classList.remove('g11');
-      tameshi.classList.remove('g11b');
-      tameshi.classList.remove('g11c');
-      tameshi.classList.remove('g7');
-    } else if (fileName == 'Galmuri7') {
-      list.classList.add('g7');
-      list.classList.remove('g14');
-      list.classList.remove('g11');
-      list.classList.remove('g11b');
-      list.classList.remove('g11c');
-      list.classList.remove('g9');
-      tameshi.classList.add('g7');
-      tameshi.classList.remove('g14');
-      tameshi.classList.remove('g11');
-      tameshi.classList.remove('g11b');
-      tameshi.classList.remove('g11c');
-      tameshi.classList.remove('g9');
-    }
-    fetch('./dist/' + fileName + '.bdf').then(async function (response) {
-      const text = await response.text();
-      if (window.Worker) {
-        const worker = new Worker('./files/worker.js');
-        worker.postMessage(text);
-        worker.onmessage = function (glphs) {
-          list.innerHTML = glphs.data;
-        }
-      } else {
-        alert('Your browser does not support Web Workers.');
-        return false;
+function viewFonts() {
+  document.getElementById('loading').style.display = 'flex';
+  for (let i = 0; i < font.length; i++) {
+    font[i].addEventListener('click', event => {
+      var fileName = font[i].innerHTML;
+      if (fileName == 'Galmuri14') {
+        list.classList.add('g14');
+        list.classList.remove('g11');
+        list.classList.remove('g11b');
+        list.classList.remove('g11c');
+        list.classList.remove('g9');
+        list.classList.remove('g7');
+        tameshi.classList.add('g14');
+        tameshi.classList.remove('g11');
+        tameshi.classList.remove('g11b');
+        tameshi.classList.remove('g11c');
+        tameshi.classList.remove('g9');
+        tameshi.classList.remove('g7');
+      } else if (fileName == 'Galmuri11') {
+        list.classList.add('g11');
+        list.classList.remove('g14');
+        list.classList.remove('g11b');
+        list.classList.remove('g11c');
+        list.classList.remove('g9');
+        list.classList.remove('g7');
+        tameshi.classList.add('g11');
+        tameshi.classList.remove('g14');
+        tameshi.classList.remove('g11b');
+        tameshi.classList.remove('g11c');
+        tameshi.classList.remove('g9');
+        tameshi.classList.remove('g7');
+      } else if (fileName == 'Galmuri11 Bold') {
+        var fileName = 'Galmuri11-Bold';
+        list.classList.add('g11b');
+        list.classList.remove('g14');
+        list.classList.remove('g11');
+        list.classList.remove('g11c');
+        list.classList.remove('g9');
+        list.classList.remove('g7');
+        tameshi.classList.add('g11b');
+        tameshi.classList.remove('g14');
+        tameshi.classList.remove('g11');
+        tameshi.classList.remove('g11c');
+        tameshi.classList.remove('g9');
+        tameshi.classList.remove('g7');
+      } else if (fileName == 'Galmuri11 Condensed') {
+        var fileName = 'Galmuri11-Condensed';
+        list.classList.add('g11c');
+        list.classList.remove('g14');
+        list.classList.remove('g11');
+        list.classList.remove('g11b');
+        list.classList.remove('g9');
+        list.classList.remove('g7');
+        tameshi.classList.add('g11c');
+        tameshi.classList.remove('g14');
+        tameshi.classList.remove('g11');
+        tameshi.classList.remove('g11b');
+        tameshi.classList.remove('g9');
+        tameshi.classList.remove('g7');
+      } else if (fileName == 'Galmuri9') {
+        list.classList.add('g9');
+        list.classList.remove('g14');
+        list.classList.remove('g11');
+        list.classList.remove('g11b');
+        list.classList.remove('g11c');
+        list.classList.remove('g7');
+        tameshi.classList.add('g9');
+        tameshi.classList.remove('g14');
+        tameshi.classList.remove('g11');
+        tameshi.classList.remove('g11b');
+        tameshi.classList.remove('g11c');
+        tameshi.classList.remove('g7');
+      } else if (fileName == 'Galmuri7') {
+        list.classList.add('g7');
+        list.classList.remove('g14');
+        list.classList.remove('g11');
+        list.classList.remove('g11b');
+        list.classList.remove('g11c');
+        list.classList.remove('g9');
+        tameshi.classList.add('g7');
+        tameshi.classList.remove('g14');
+        tameshi.classList.remove('g11');
+        tameshi.classList.remove('g11b');
+        tameshi.classList.remove('g11c');
+        tameshi.classList.remove('g9');
       }
+      fetch('./dist/' + fileName + '.bdf').then(async function (res) {
+        const text = await res.text();
+        if (window.Worker) {
+          const worker = new Worker('./files/worker.js');
+          worker.postMessage(text);
+          worker.onmessage = function (glphs) {
+            list.innerHTML = glphs.data;
+          }
+        } else {
+          alert('Your browser does not support Web Workers.');
+          document.getElementById('loading').style.display = 'none';
+          return false;
+        }
+      });
     });
-  });
+  }
+  document.getElementById('loading').style.display = 'none';
 }
+
+viewFonts();
 
 var HUDNotification = {
   el: document.getElementById('hud-notification'),
