930ae29280d2d6eff7254a3c7aa68ebcef9537f1
Lee Minseo <itoupluk427@gmail.com>
2023-04-18 21:56:03 +0900

    v2.36.3
    * Galmuri11 Bold: 라틴 문자 자형 업데이트

--- files/updateChar.py
+++ files/updateChar.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-import os
 import re
 from bs4 import BeautifulSoup
+from os.path import dirname, abspath
 
 fonts = ['Galmuri14', 'Galmuri11', 'Galmuri11-Bold', 'Galmuri11-Condensed', 'Galmuri9', 'Galmuri7', 'GalmuriMono11', 'GalmuriMono7']
 chars = ['hangul', 'ksx1001', 'ksc5601', 'cp949', 'jis0201', 'jis0208', 'jis0212', 'shiftjis', 'cp932']
 styles = ''
 
 for font in fonts:
-  with open(os.getcwd() + '/dist/' + font + '.bdf', 'rt', encoding='utf8') as f:
+  with open(dirname(dirname(abspath(__file__))) + '/dist/' + font + '.bdf', 'rt', encoding='utf8') as f:
     fontArr = re.findall(r'^STARTCHAR U\+([\d\w]{4})', f.read(), re.MULTILINE)
 
     for char in chars:
-      with open(os.getcwd() + '/files/charsets/' + char + '.txt', 'rt', encoding='utf8') as c:
+      with open(dirname(abspath(__file__)) + '/charsets/' + char + '.txt', 'rt', encoding='utf8') as c:
         if char == 'hangul':
           charArr = re.findall(r'^\d{1,5}\t(?:-|[\d\w]{4})\t(?:-|[\d\w]{4})\t(?:-|[\d\w]{4})\t(?:-|[\d\w]{4})\t(?:-|[\d\w]{4})\t([\d\w]{4})', c.read(), re.MULTILINE)
         else:
@@ -21,18 +21,18 @@ for font in fonts:
         available = len(set(fontArr) & set(charArr))
         all = len(set(charArr))
 
-        with open(os.getcwd() + '/charsets.html', 'rt', encoding='utf8') as h:
+        with open(dirname(dirname(dirname(abspath(__file__)))) + '/galmuri-site/charsets.html', 'rt', encoding='utf8') as h:
           bs = BeautifulSoup(h.read(), 'html.parser')
           bs.select_one('#' + font + '_' + char + '>span:first-child').string = str(available) + ' / ' + str(all)
           bs.select_one('#' + font + '_' + char + '>span:last-child').string = str(round(available / all * 100, 3)) + ' %'
           styles += '#' + font + '_' + char + '::before{width:' + str(available / all * 100) + '%}'
 
-          with open(os.getcwd() + '/charsets.html', 'wt', encoding='utf8') as h:
+          with open(dirname(dirname(dirname(abspath(__file__)))) + '/galmuri-site/charsets.html', 'wt', encoding='utf8') as h:
             h.write(str(bs))
 
-with open(os.getcwd() + '/charsets.html', 'rt', encoding='utf8') as h:
+with open(dirname(dirname(dirname(abspath(__file__)))) + '/galmuri-site/charsets.html', 'rt', encoding='utf8') as h:
   bs = BeautifulSoup(h.read(), 'html.parser')
   bs.select_one('#progress').string = styles
 
-  with open(os.getcwd() + '/charsets.html', 'wt', encoding='utf8') as h:
+  with open(dirname(dirname(dirname(abspath(__file__)))) + '/galmuri-site/charsets.html', 'wt', encoding='utf8') as h:
     h.write(str(bs))
