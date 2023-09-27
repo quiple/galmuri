be5c516f202f4b4f5f75eba050b8671650fab56c
Minseo Lee <itoupluk427@gmail.com>
2023-09-27 13:45:19 +0900

    v2.388

--- files/versionFea.py
+++ files/versionFea.py
@@ -7,8 +7,10 @@ b = sys.argv[1]
 f = sys.argv[2]
 
 with open(b, 'rt', encoding='utf8') as before:
-  mod = re.findall(r'FontRevision\s+(\d\.\d{3})', before.read())
+  mod = re.findall(r'FontRevision\s+\d\.\d{3}', before.read())
 
-with open(f, 'wt', encoding='utf8') as after:
-  ver = re.sub(r'FontRevision\s+(\d\.\d{3})', mod, after.read())
-  after.write(ver)
+with open(f, 'rt', encoding='utf8') as after:
+  ver = re.sub(r'FontRevision\s+(\d\.\d{3})', mod[0], after.read())
+
+with open(f, 'wt', encoding='utf8') as write:
+  write.write(ver)
